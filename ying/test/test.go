7300710e4e65bed4c32b2a0c4dbf18e0f3b3132c
ying <1625778735@qq.com>
2021-11-12 17:20:16 +0800

    新增并完成并发相关

--- ying/test/test.go
+++ ying/test/test.go
@@ -1,27 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type Person struct {
-	id   int
-	name string
-}
+/**
+使用并发模拟接力比赛
+*/
 
-func (p Person) Print() {
-	fmt.Println(p)
-}
-
-func (p *Person) setId(id int) {
-	p.id = id
-}
+var wg sync.WaitGroup
 
 func main() {
-	p := Person{
-		id:   1,
-		name: "郑晓颖",
-	}
+	wg.Add(1)
+	ch := make(chan int)
+	go Runner(ch)
 
-	p.Print()
-	p.setId(0)
+	//比赛开始
+	ch <- 1
+	wg.Wait()
+}
+
+func Runner(ch chan int) {
+	defer wg.Done()
+	runner := <-ch
+	fmt.Printf("运动员%d号接棒\n", runner)
+	fmt.Printf("运动员%d号跑起来了\n", runner)
+	if runner < 4 {
+		newRunner := runner + 1
+		fmt.Printf("运动员%d号已就位\n", newRunner)
+		wg.Add(1)
+		go Runner(ch)
+
+		fmt.Printf("运动员%d号开始传棒\n", runner)
+		ch <- newRunner
+	} else {
+		fmt.Printf("运动员%d号冲过终点\n", runner)
+		fmt.Println("比赛结束！")
+		close(ch)
+	}
 
 }
