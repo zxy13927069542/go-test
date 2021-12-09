07120a0677f5acda59d1f18381d557ff5764de94
zxy <zhengxiaoying666@gmail.com>
2021-12-09 21:34:07 +0800

    更新strings.Index()的使用及坑

--- ying/test/test.go
+++ ying/test/test.go
@@ -1,8 +1,20 @@
-package test
+package main
 
-type Supplier struct {
-	Id          int    //主键编码
-	Code        string //供应商编码
-	Name        string //名称
-	Description string //描述
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	//°: 2个字节 ′: 三个字节 ″: 三个字节
+	s := `300°330′330″`
+
+	//验证这几个字符占用几个字节
+	//bytes := []byte(s)
+	//fmt.Println(bytes)
+
+	index0 := strings.Index(s, "°")
+	index1 := strings.Index(s, "′") - 1
+	index2 := strings.Index(s, "″") - 1 - 2
+	fmt.Println(index0, index1, index2)
 }
