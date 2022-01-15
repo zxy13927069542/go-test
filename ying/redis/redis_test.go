2a664cabd13fceabb8809681bf12415292c480fa
zxy <zhengxiaoying666@gmail.com>
2022-01-15 16:30:33 +0800

    新增redis测试

--- ying/redis/redis_test.go
+++ ying/redis/redis_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+var client *redis.Client
+
 //
 // GetRedisClient
 //  @Description: 创建redis客户端
@@ -33,6 +35,66 @@ func GetRedisClient() *redis.Client {
 	return client
 }
 
+func InitClient() {
+	client = GetRedisClient()
+}
+
 func TestConnect(t *testing.T) {
-	GetRedisClient()
+	InitClient()
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func TestSet(t *testing.T) {
+	InitClient()
+	err := client.Set("s1", "hello", 0).Err()
+	if err != nil {
+		log.Errorf("Set s1 Failed! Reason: %v", err)
+		return
+	}
+
+	s1 := client.Get("s1").String()
+	log.Infof("Get s1 Success! s1: %v", s1)
+
+}
+
+//
+//  TestSetStruct
+//  @Description: 测试set结构体,失败，无法set结构体
+//
+func TestSetStruct(t *testing.T) {
+	InitClient()
+	//  测试set结构体
+	//  失败
+	var p1 Person
+	p1.Name = "郑晓颖"
+	p1.Age = 18
+	err := client.Set("s2", p1, 0).Err()
+	if err != nil {
+		log.Errorf("Set s2 Failed! Reason: %v", err)
+		return
+	}
+
+	s2 := client.Get("s2").String()
+	log.Infof("Get s2 Success! s1: %v", s2)
+}
+
+//
+//  TestSetNX
+//  @Description: 测试set nx
+//  @param t
+//
+func TestSetNX(t *testing.T) {
+	InitClient()
+	//  测试nx
+	success := client.SetNX("s1", "nx", 0).Val()
+	if !success {
+		log.Errorf("SetNX s1 Failed!")
+	}
+
+	s1 := client.Get("s1").String()
+	log.Infof("Get s1 Success! s1: %v", s1)
 }
